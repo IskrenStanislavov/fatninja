cd6ac2cb8a372343d03a9133d2bf2ef1732e120a
Iskren.Stanislavov <iskren.jgd@gmail.com>
2016-04-27 21:28:13 +0300

    fix err in animation.js

--- libs/animation.js
+++ libs/animation.js
@@ -2,7 +2,7 @@ define(function(require) {
 	var PIXI        = require("libs/pixi");
 	var Animation = function(settings){
 		PIXI.MovieClip.call(this);
-		PIXI.MovieClip.fromFrames([].range()))
+		PIXI.MovieClip.fromFrames([].range());
 		this.idle = this.addChild(PIXI.Sprite.fromFrame("idle/idle (1).png"));
 		this.idle.anchor.set(0.5,0.5);
 		this.animation = this.addChild(new CharacterAnimation());
